d39545a1a6029bf931ab39f67b1574488e651f2a
saarthaksalgotra <133770011+saarthaksalgotra@users.noreply.github.com>
2023-05-16 21:31:04 +0530

    Add files via upload

---
+++ saarthak/stringrev.java
@@ -0,0 +1,9 @@
+package String;
+
+public class stringrev {
+    public static void main(String[] args) {
+        StringBuffer sb=new StringBuffer("Sky is Blue");
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
